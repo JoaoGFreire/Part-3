1c00b0a599a6f00e1c87107c6a1ff2c9baf2527e
PC-JOAO\joaog <joaogfff@gmail.com>
2024-03-14 21:54:08 -0400

    Completed Task 4b

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,10 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMP_Text villagerType;
+    //public ChestType type;
     public static Villager SelectedVillager { get; private set; }
+    private void Update()
+    {
+
+        villagerType.text = "none selected";
+        if (SelectedVillager.CanOpen() == ChestType.Merchant)
+        {
+            villagerType.text = "Merchant";
+        }
+        if(SelectedVillager.CanOpen() == ChestType.Archer)
+        {
+            villagerType.text = "Archer";
+        }
+        if(SelectedVillager.CanOpen() == ChestType.Thief)
+        {
+            villagerType.text = "Thief";
+        }
+        
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -15,5 +37,6 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
+    
     
 }
