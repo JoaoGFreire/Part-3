a625dbfc58669a4a84647c17e961bf229e80e96c
PC-JOAO\joaog <joaogfff@gmail.com>
2024-03-31 18:46:29 -0400

    Finished Feature 1, Menu Scene and added switching scene function that moves to the gameplay scene

---
+++ Assets/Assignment/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = (currentScene + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextScene);
+    }
+}
